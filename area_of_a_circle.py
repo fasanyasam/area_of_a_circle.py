8bec7bde45e0fc9a9f3a3588271290c51b528e27
fasanyasam <56942546+fasanyasam@users.noreply.github.com>
2020-03-26 06:41:00 +0100

    Add files via upload

---
+++ area_of_a_circle.py
@@ -0,0 +1,6 @@
+#assign a variable that accepts an input and floats the input  
+r=float(input("Enter the radius: "))
+#calculate the area of the circle with pi as 3.14
+a= 3.14*(r**2.0)
+#print result as a string concatenated with other strings
+print("The area of the circle is " + str(a))
